bc9ded88d69812547fdf31dc3b8d283d4ff3ecd5
Yusuke Shimura <shimura@systemplanet.co.jp>
2021-06-10 20:11:05 +0900

    stateの値を引数ありのコールバック関数で返し、非同期処理をかわす

--- sample_app/src/components/Omikuji.js
+++ sample_app/src/components/Omikuji.js
@@ -30,13 +30,15 @@ export default class Omikuji extends Component {
 
   // ボタンの色を変更
   changeButtonColor = (item) => {
+    console.log('★changeButtonColor★');
     const mainButton = document.getElementById("mainButton");
     mainButton.style.background = item.buttonColor;
   }
 
   // ローカルストレージに値を保存
-  saveResultOmikujiData = (history) => {
-    console.log('state2:'+JSON.stringify(this.state));
+  saveResultOmikujiData = (history) => () => {
+    console.log('★saveResultOmikujiData★');
+    console.log('stateの値:'+JSON.stringify(this.state.name));
     const item = JSON.stringify(this.state); // 今回の運勢結果を変数に格納
     const MAX_SAVE_COUNT = 10;               // ローカルストレージに保存するデータの最大数
     
@@ -47,15 +49,25 @@ export default class Omikuji extends Component {
     localStorage.setItem('omikuji-history', JSON.stringify(history)); // ローカルストレージにおみくじの結果（配列）を保存
   }
 
-  // 画面に履歴に表示
+  // 画面に履歴を表示
   drawHistory = (history) => {
+    console.log('★drawHistory★');
+    console.log('stateの値(drawHistory):'+JSON.stringify(this.state.name));
     // removeChildren(element)//一番古い履歴を削除(子ノードのリストを削除)
     //slice()は、文字列や配列などからデータの一部分だけ取り出せるメソッドになります
     //hoge.reverse() で破壊的。hoge.slice().reverse() で非破壊的.逆順にしたhistoryをrecordにいれる
     //「配列」historyの値を1つずつ「変数recordへ代入してくれるようになります。
     for (const record of history.slice().reverse()) {
-      const time = record.id;
-      const omikujiResult = record.item;
+      const { id, item } = record;
+      // console.log('record:'+record);
+      // console.log('id:'+id);
+      // console.log('item:'+item.name);
+
+      // const time = record.id;
+      // const omikujiResult = JSON.stringify(JSON.parse(record.item));
+      // console.log(omikujiResult.name);
+      // const redordLst = omikujiResult + time; 
+      // console.log(redordLst);
       //console.log(time);
       //console.log(omikujiResult);
 
@@ -93,33 +105,19 @@ export default class Omikuji extends Component {
           comment: item.comment,
           probability: item.probability,
           buttonColor: item.buttonColor
-        });
-
-        // //stete2
-        // this.setState((state) => {
-        //   // 重要：更新には `this.state` ではなく `state` を使います。
-        //   return {
-        //     name: item.name,
-        //     comment: item.comment,
-        //     probability: item.probability,
-        //     buttonColor: item.buttonColor
-        //   }
-        // });
-
-        console.log('state00:'+item.name);
-        console.log('state0:'+JSON.stringify(this.state));
-
+        },
+        this.saveResultOmikujiData(history) // stateは非同期処理なのでコールバック関数として第２引数に設定。
+        );
         // 【1】ボタンの色を変更
         this.changeButtonColor(item);
         break;
       }
     }
-    console.log('state1:'+JSON.stringify(this.state));
     // 【2】ローカルストレージに値を保存
-    this.saveResultOmikujiData(history);
+    //this.saveResultOmikujiData(history);
     // 【3】履歴にローカルストレージの値を表示
     this.drawHistory(history);
-  } 
+  }
 }
 
 // CSS in Js
